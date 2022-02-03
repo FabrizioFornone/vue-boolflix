235cabaf6fc7f69b01eeb920cdc80cd48a9d00c8
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-02-03 16:31:55 +0100

    trying to create an internal component: cardBox.vue

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -2,31 +2,8 @@
   <main>
     <div class="container py-4">
       <div class="row flex-wrap">
-        <div
-          class="movie col-2 my-2"
-          v-for="movie in moviesProp"
-          :key="movie.id"
-        >
-          <div class="hover-div border border-white py-4 px-3">
-            <div><strong>Titolo:</strong> {{ movie.title }}</div>
-            <div>
-              <strong>Titolo originale:</strong> {{ movie.original_title }}
-            </div>
-            <div><strong>Voto:</strong> {{ movie.vote_average }}</div>
-            <div><strong>Overview:</strong> {{ movie.overview }}</div>
-          </div>
-          <a href="#">
-            <img
-              v-if="movie.poster_path !== null"
-              :src="`https://www.themoviedb.org/t/p/original${movie.poster_path}`"
-              alt=""
-            />
-            <img
-              v-else
-              src="https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg"
-              alt=""
-            />
-          </a>
+        <div v-for="movie in moviesProp" :key="movie.id">
+          <card-box :card="moviesProp" />
         </div>
       </div>
     </div>
@@ -34,7 +11,9 @@
 </template>
 
 <script>
+import cardBox from "./cardBox.vue";
 export default {
+  components: { cardBox },
   props: {
     moviesProp: Array,
   },
---
+++ src/components/cardBox.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="movie col-2 my-2">
+      <div class="hover-div border border-white py-4 px-3">
+        <div><strong>Titolo:</strong> {{ card.title }}</div>
+        <div><strong>Titolo originale:</strong> {{ card.original_title }}</div>
+        <div><strong>Voto:</strong> {{ card.vote_average }}</div>
+        <div><strong>Overview:</strong> {{ card.overview }}</div>
+      </div>
+      <a href="#">
+        <img
+          :src="`https://www.themoviedb.org/t/p/original${card.poster_path}`"
+          alt=""
+        />
+        <!-- <img
+          v-else
+          src="https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg"
+          alt=""
+        /> -->
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    card: Object,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/style/cardstyle.scss";
+</style>
