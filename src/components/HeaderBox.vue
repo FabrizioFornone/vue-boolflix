4ca9736a6f976bfe29aee6d0eb4fe50f883b6faa
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-02-03 11:45:51 +0100

    repo created + HeaderBox.vue component created + headerStyle.scss created

---
+++ src/components/HeaderBox.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <div class="d-flex justify-content-between align-items-center pt-3 pb-2">
+      <h2 class="text-uppercase mx-4">boolflix</h2>
+      <div>
+        <input id="search-input" class="mx-4" type="text" placeholder="Search" />
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import "@/style/headerStyle.scss";
+</style>
