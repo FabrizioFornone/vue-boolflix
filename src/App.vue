d1be31df4db9fa899efe4a731c3274a8e03a91bc
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-02-03 11:55:02 +0100

    MainComponent.vue component created

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
     <header-box />
+    <main-component />
   </div>
 </template>
 
 <script>
 import HeaderBox from "./components/HeaderBox.vue";
+import MainComponent from "./components/MainComponent.vue";
 
 export default {
   name: "App",
-  components: { HeaderBox },
+  components: { HeaderBox, MainComponent },
 };
 </script>
 
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <div>test</div>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import "@/style/mainComponentStyle.scss";
+</style>
