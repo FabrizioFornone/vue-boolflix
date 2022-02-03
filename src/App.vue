e7d23ec3283de0bad84ac3584b27375e4395de3a
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-02-03 22:42:50 +0100

    shuffleResult argument changed

--- src/App.vue
+++ src/App.vue
@@ -49,16 +49,16 @@ export default {
         recipientArray.push(element);
       });
     },
-    shuffleResult(array) {
-      for (let i = array.length - 1; i > 0; i--) {
+    shuffleResult(arrayToShuffle) {
+      for (let i = arrayToShuffle.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
 
-        const temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+        const temp = arrayToShuffle[i];
+        arrayToShuffle[i] = arrayToShuffle[j];
+        arrayToShuffle[j] = temp;
       }
 
-      return array;
+      return arrayToShuffle;
     },
   },
 };
