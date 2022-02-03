5437b87276f6f6b7c70233ade112c6c97200d9cd
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-02-03 14:16:35 +0100

    .hover-div created + hover selector implemented

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -2,7 +2,19 @@
   <main>
     <div class="container py-4">
       <div class="row flex-wrap">
-        <div class="col-2 my-2" v-for="movie in moviesProp" :key="movie.id">
+        <div
+          class="movie col-2 my-2"
+          v-for="movie in moviesProp"
+          :key="movie.id"
+        >
+          <div class="hover-div py-4 px-3">
+            <div><strong>Titolo:</strong> {{ movie.title }}</div>
+            <div>
+              <strong>Titolo originale:</strong> {{ movie.original_title }}
+            </div>
+            <div><strong>Voto:</strong> {{ movie.vote_average }}</div>
+            <div><strong>Overview:</strong> {{ movie.overview }}</div>
+          </div>
           <a href="#">
             <img
               :src="`https://www.themoviedb.org/t/p/w300_and_h450_bestv2${movie.poster_path}`"
