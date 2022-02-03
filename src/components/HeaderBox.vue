15a552bd12d6cd2be84c386c86cfd2f9ddb1b0cc
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-02-03 12:03:24 +0100

    developing HeaderBox.vue component

--- src/components/HeaderBox.vue
+++ src/components/HeaderBox.vue
@@ -3,7 +3,8 @@
     <div class="d-flex justify-content-between align-items-center pt-3 pb-2">
       <h2 class="text-uppercase mx-4">boolflix</h2>
       <div>
-        <input id="search-input" class="mx-4" type="text" placeholder="Search" />
+        <input id="search-input" class="mx-2 py-1 px-2 border-white rounded-pill" type="text" placeholder="Search" />
+        <button id="search-btn" class="mx-4 py-1 px-2 border-white rounded-pill">Start!</button>
       </div>
     </div>
   </header>
