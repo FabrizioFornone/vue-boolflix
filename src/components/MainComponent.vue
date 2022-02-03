296799b3afeb7ce0711f486202b055ec0ce63263
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-02-03 16:02:39 +0100

    .img-not-found css class deleted

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -22,7 +22,6 @@
               alt=""
             />
             <img
-              class="img-not-found"
               v-else
               src="https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg"
               alt=""
