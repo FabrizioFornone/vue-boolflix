25d033036dc5cd8a4d2616f24405e26bb45f41d2
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-02-03 12:55:22 +0100

    callApi dynamic method done

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
   <div id="app">
-    <header-box />
+    <header-box @search="callApi" />
     <main-component />
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import HeaderBox from "./components/HeaderBox.vue";
 import MainComponent from "./components/MainComponent.vue";
 
 export default {
   name: "App",
   components: { HeaderBox, MainComponent },
+  data() {
+    return {
+      moviesFound: [],
+    };
+  },
+  methods: {
+    callApi(keyword) {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/search/movie/?api_key=fbf42efdae098c0577337b304561e7e9&query=${keyword}`
+        )
+        .then((response) => {
+          this.moviesFound = response.data.results;
+        });
+    },
+  },
 };
 </script>
 
--- src/components/HeaderBox.vue
+++ src/components/HeaderBox.vue
@@ -3,15 +3,33 @@
     <div class="d-flex justify-content-between align-items-center pt-3 pb-2">
       <h2 class="text-uppercase mx-4">boolflix</h2>
       <div>
-        <input id="search-input" class="mx-2 py-1 px-2 border-white rounded-pill" type="text" placeholder="Search" />
-        <button id="search-btn" class="mx-4 py-1 px-2 border-white rounded-pill">Start!</button>
+        <input
+          id="search-input"
+          class="mx-2 py-1 px-2 rounded-pill"
+          type="text"
+          placeholder="Search"
+          v-model="keywordSearch"
+        />
+        <button
+          id="search-btn"
+          class="mx-4 py-1 px-2 rounded-pill"
+          @click="$emit('search', keywordSearch)"
+        >
+          Start!
+        </button>
       </div>
     </div>
   </header>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      keywordSearch: "",
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
