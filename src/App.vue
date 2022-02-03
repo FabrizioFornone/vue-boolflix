c72d010cb2a749f82300eea5f7b0147196d81615
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-02-03 21:20:34 +0100

    CallApis new method implemented

--- src/App.vue
+++ src/App.vue
@@ -15,43 +15,36 @@ export default {
   components: { HeaderBox, MainComponent },
   data() {
     return {
-      moviesFound: [],
-      tvSeriesFound: [],
       sumResults: [],
     };
   },
   methods: {
-    callApiFilm(keyword) {
+    callApis(keyword) {
       if (keyword === "") {
         this.moviesFound = [];
       } else {
-        axios
-          .get(
-            `https://api.themoviedb.org/3/search/movie/?api_key=fbf42efdae098c0577337b304561e7e9&query=${keyword}`
-          )
-          .then((response) => {
-            this.moviesFound = response.data.results;
-          });
+        this.sumResults = [];
+        let multiAxios = [
+          `https://api.themoviedb.org/3/search/movie/?api_key=fbf42efdae098c0577337b304561e7e9&query=${keyword}`,
+          `https://api.themoviedb.org/3/search/tv/?api_key=fbf42efdae098c0577337b304561e7e9&query=${keyword}`,
+        ];
+        let array = [];
+        Promise.all(multiAxios.map((endpoint) => axios.get(endpoint))).then(
+          axios.spread((...allData) => {
+            allData.forEach((data) => {
+              array.push(data.data.results);
+            });
+            const [array1, array2] = array;
+            array1.forEach((element) => {
+              this.sumResults.push(element);
+            });
+            array2.forEach((element) => {
+              this.sumResults.push(element);
+            });
+            this.shuffleResult(this.sumResults);
+          })
+        );
       }
-    },
-    callApiSeries(keyword) {
-      if (keyword === "") {
-        this.tvSeriesFound = [];
-      } else {
-        axios
-          .get(
-            `https://api.themoviedb.org/3/search/tv/?api_key=fbf42efdae098c0577337b304561e7e9&query=${keyword}`
-          )
-          .then((response) => {
-            this.tvSeriesFound = response.data.results;
-          });
-      }
-    },
-    callApis(keywordApi) {
-      this.callApiFilm(keywordApi);
-      this.callApiSeries(keywordApi);
-      this.sumResults = [...this.moviesFound, ...this.tvSeriesFound];
-      this.sumResults = this.shuffleResult(this.sumResults);
     },
     shuffleResult(array) {
       for (let i = array.length - 1; i > 0; i--) {
