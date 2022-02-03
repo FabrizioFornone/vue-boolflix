f7e59baaded6f7b1da565534462aa23b18bb74e8
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-02-03 12:10:44 +0100

    MainComponent.vue static template done

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,6 +1,45 @@
 <template>
   <main>
-    <div>test</div>
+    <div class="container py-4">
+      <div class="row flex-wrap">
+        <div class="col-2">
+          <img
+            src="https://c8.alamy.com/compit/e5n1d2/goodfellas-ray-liotta-robert-deniro-joe-pesci-1990-warner-bros-cortesia-everett-collection-e5n1d2.jpg"
+            alt=""
+          />
+        </div>
+        <div class="col-2">
+          <img
+            src="https://c8.alamy.com/compit/e5n1d2/goodfellas-ray-liotta-robert-deniro-joe-pesci-1990-warner-bros-cortesia-everett-collection-e5n1d2.jpg"
+            alt=""
+          />
+        </div>
+        <div class="col-2">
+          <img
+            src="https://c8.alamy.com/compit/e5n1d2/goodfellas-ray-liotta-robert-deniro-joe-pesci-1990-warner-bros-cortesia-everett-collection-e5n1d2.jpg"
+            alt=""
+          />
+        </div>
+        <div class="col-2">
+          <img
+            src="https://c8.alamy.com/compit/e5n1d2/goodfellas-ray-liotta-robert-deniro-joe-pesci-1990-warner-bros-cortesia-everett-collection-e5n1d2.jpg"
+            alt=""
+          />
+        </div>
+        <div class="col-2">
+          <img
+            src="https://c8.alamy.com/compit/e5n1d2/goodfellas-ray-liotta-robert-deniro-joe-pesci-1990-warner-bros-cortesia-everett-collection-e5n1d2.jpg"
+            alt=""
+          />
+        </div>
+        <div class="col-2">
+          <img
+            src="https://c8.alamy.com/compit/e5n1d2/goodfellas-ray-liotta-robert-deniro-joe-pesci-1990-warner-bros-cortesia-everett-collection-e5n1d2.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
