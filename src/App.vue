a96abd7fb6d3df537fbe7e4d4e2fd042c3cef24f
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-02-07 16:18:18 +0100

    errata corrige in a comment

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@ export default {
     };
   },
   methods: {
-    // method for shuffling elements into an arraya
+    // method for shuffling elements into an array
     shuffleResult(array) {
       for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
