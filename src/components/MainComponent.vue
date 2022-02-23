b67949e8a8ce826956dbd1102f08b76a569fb46a
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-02-23 22:48:52 +0100

    fix

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -7,7 +7,7 @@
           v-for="element in resultsProp"
           :key="element.id"
         >
-        <!-- Card Component -->
+        <!-- Card Component  -->
           <card-box
             :card="element"
             :flagsPropCard="flagsProp"
