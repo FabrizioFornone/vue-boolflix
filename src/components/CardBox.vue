134d80764b439ee0a90cf30ba4420dce98469dfb
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-02-23 22:52:05 +0100

    card renamed loaded

---
+++ src/components/CardBox.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="card-box">
+    <!-- Hover template -->
+    <div class="hover-div py-4 px-3">
+      <!-- Title -->
+      <div class="py-1">
+        <strong> <span class="me-1">Titolo:</span> </strong>
+        {{ titleProp }} {{ nameProp }}
+      </div>
+      <!-- Original Title -->
+      <div class="py-1">
+        <strong> <span class="me-1">Titolo originale:</span> </strong>
+        <span> {{ originalTitleProp }} {{ originalNameProp }} </span>
+      </div>
+      <!-- Vote -->
+      <div class="d-flex py-1">
+        <strong> <span class="me-1">Voto:</span> </strong>
+        <rating-stars :rateNumber="card.vote_average" />
+      </div>
+      <!-- Language -->
+      <div class="py-1">
+        <strong>Lingua:</strong>
+        <span
+          class="mx-2"
+          v-if="langCheck(flagsPropCard, card.original_language)"
+        >
+          <strong>{{ card.original_language }}</strong>
+        </span>
+        <img
+          v-else
+          class="flag mx-2"
+          :src="`/flags/${card.original_language}.png`"
+          alt=""
+        />
+      </div>
+      <!-- Overview -->
+      <div class="py-1">
+        <strong> <span class="me-1">Overview:</span> </strong>
+        <span>{{ card.overview }}</span>
+      </div>
+    </div>
+    <a href="#">
+      <!-- Poster -->
+      <img
+        class="rounded"
+        v-if="card.poster_path !== null"
+        :src="`https://www.themoviedb.org/t/p/original${card.poster_path}`"
+        alt=""
+      />
+      <!-- Poster IMG not found -->
+      <img
+        class="rounded"
+        v-else
+        src="https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg"
+        alt=""
+      />
+    </a>
+  </div>
+</template>
+
+<script>
+import RatingStars from "./RatingStars.vue";
+export default {
+  components: { RatingStars },
+  props: {
+    card: Object,
+    flagsPropCard: Array,
+    titleProp: String,
+    nameProp: String,
+    originalTitleProp: String,
+    originalNameProp: String,
+  },
+  // Method that checks if the language is present between the flags
+  methods: {
+    langCheck(flag, lang) {
+      return !flag.includes(lang);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/style/cardstyle.scss";
+</style>
---
+++ src/components/RatingStars.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <i class="star fas fa-star" v-for="(star, index) in stars()" :key="index">
+    </i>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    rateNumber: Number,
+  },
+  methods: {
+    stars() {
+      return Math.round(this.rateNumber / 2);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.star {
+  margin: 0 1px;
+  font-size: 11px;
+}
+</style>
